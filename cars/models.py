7b97ca5fa6051aad5f1fc16b99219b6a56671a73
Leonardo <leo.sales.br@gmail.com>
2026-04-15 10:11:26 -0300

    models

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Criando a Tabela.
+class Car(models.Model):
+    id = models.AutoField(primary_key=True) # AutoField é um campo de auto incremento /número único para cada registro.
+    model = models.CharField(max_length=200) # CharField é um campo de texto, max_length é o tamanho máximo do texto.
+    brand = models.CharField(max_length=200) # CharField é um campo de texto, max_length é o tamanho máximo do texto.
+    factory_year = models.IntegerField(blank=True, null=True) # IntegerField é um campo de número inteiro.
+    year = models.IntegerField(blank=True, null=True) # IntegerField é um campo de número inteiro.
+    value = models.FloatField(blank=True, null=True) # FloatField é um campo de número decimal, blank=True permite que o campo seja deixado em branco, null=True permite que o campo seja nulo.
+
