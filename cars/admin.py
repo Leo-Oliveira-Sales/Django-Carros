4316c7987dfde3441d149dbe3e85846b24d2af7f
Leonardo <leo.sales.br@gmail.com>
2026-04-15 13:51:57 -0300

    Brand

--- cars/admin.py
+++ cars/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
 
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',) # list_display define os campos que serão exibidos na lista de objetos no admin.
+    search_fields = ('name',) # search_fields define os campos que serão pesquisados quando o usuário usar a barra de pesquisa no admin.
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value') # list_display define os campos que serão exibidos na lista de objetos no admin.
     search_fields = ('model', 'brand') # search_fields define os campos que serão pesquisados quando o usuário usar a barra de pesquisa no admin.
 
-admin.site.register(Car, CarAdmin) # admin.site.register registra o modelo Car para aparecer no admin
 
+admin.site.register(Brand, BrandAdmin) # admin.site.register registra o modelo Brand para aparecer no admin
+admin.site.register(Car, CarAdmin) # admin.site.register registra o modelo Car para aparecer no admin
--- cars/models.py
+++ cars/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 # Criando a Tabela.
+
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True) # AutoField é um campo de auto incremento /número único para cada registro.
+    name = models.CharField(max_length=200) # CharField é um campo de texto, max_length é o tamanho máximo do texto.
+
+    def __str__(self):
+        return self.name # Vai retornar o nome da marca, e não mais Brand object (1), Brand object (2) etc.
+
+
 class Car(models.Model):
     id = models.AutoField(primary_key=True) # AutoField é um campo de auto incremento /número único para cada registro.
     model = models.CharField(max_length=200) # CharField é um campo de texto, max_length é o tamanho máximo do texto.
-    brand = models.CharField(max_length=200) # CharField é um campo de texto, max_length é o tamanho máximo do texto.
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand') # ForeignKey é um campo de chave estrangeira, on_delete=models.PROTECT impede que a marca seja deletada se houver um carro associado a ela, related_name é o nome do relacionamento.
     factory_year = models.IntegerField(blank=True, null=True) # IntegerField é um campo de número inteiro.
     model_year = models.IntegerField(blank=True, null=True) # IntegerField é um campo de número inteiro.
     value = models.FloatField(blank=True, null=True) # FloatField é um campo de número decimal, blank=True permite que o campo seja deixado em branco, null=True permite que o campo seja nulo.
